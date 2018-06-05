248a08edfbdbfa65e19e2b961c3144b42b5cb8b2
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-05 21:11:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function subtract(a,b){
   return a-b
 }
 
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
 
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
