c139d5367276573ec30b6faf02b3741a4ad3c00e
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-05 21:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ function divide(a,b){
 }
 
 
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
