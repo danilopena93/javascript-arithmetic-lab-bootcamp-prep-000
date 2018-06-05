6693f2a89efe9eb1458cd38073b62766676577f2
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-05 21:13:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,25 +23,18 @@ function dec(n){
   return n--
 }
 
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
-
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
-
-it('divide(a, b) divides a by b and returns the result', function() {
-  expect(divide(a, b)).toEqual(a / b)
-})
-
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
 
 it('dec(n) decrements n and returns the result', function() {
   expect(dec(a)).toEqual(a - 1)
